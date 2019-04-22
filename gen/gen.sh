1172c14d7e99c9ac6a0db3226f92eae69def9d17
Ray Stubbs <stubbs.ray@gmail.com>
2019-04-22 05:10:51 +0200

    Add libten-0.3.0

--- gen/gen.sh
+++ gen/gen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PRO=$1
 URL=https://github.com/ten-lang/$PRO/
-TAG=$(git ls-remote --tags $URL | sed "s/\n.*//" | sed 's/.*\///')
+TAG=$(git ls-remote --tags $URL | sort -r | awk 'FNR == 1' | sed 's/.*\///')
 VER=$(echo $TAG | sed 's/v//')
 GEN=$(pwd)
 LOG=$(cat rpm/$PRO/changelog.txt)
@@ -19,7 +19,7 @@ SPEC=$(eval "echo \"$SPEC\"")
 printf "%s" "$SPEC" > SPECS/$PRO-$VER-$REL.spec
 
 rm -rf SOURCES/*
-wget -O SOURCES/$TAG.tar.gz $URL/archive/$TAG.tar.gz
+wget -O "SOURCES/$TAG.tar.gz" "$URL/archive/$TAG.tar.gz"
 rpmbuild -bb -D "_topdir $(pwd)" --buildroot $(pwd)/BUILDROOT SPECS/$PRO-$VER-$REL.spec
 
 rpm --addsign --define="_gpg_name ten-lang" RPMS/*/$PRO-$VER-$REL*.rpm
