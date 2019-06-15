ba689977478b1d60e55e6155ff652244a1d5ca80
Ray Stubbs <stubbs.ray@gmail.com>
2019-06-15 16:57:06 -0500

    Update repo installation

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -O /etc/yum.repos.d/ten-lang.repo https://raw.githubusercontent.com/ten-lang/rpm-repo/master/ten-lang.repo
-wget -O -  https://raw.githubusercontent.com/ten-lang/rpm-repo/master/ten-lang.pub | gpg --import
+wget -q -O /etc/yum.repos.d/ten-lang.repo https://raw.githubusercontent.com/ten-lang/rpm-repo/master/ten-lang.repo
+wget -q -O -  https://raw.githubusercontent.com/ten-lang/rpm-repo/master/ten-lang.pub | gpg --import
